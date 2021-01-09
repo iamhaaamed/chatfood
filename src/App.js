5e33ed86e98b258cdce4a5863710b1dc362c91e2
Hamed <Hamed>
2021-01-09 08:03:34 +0330

    add google analyitcs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { createBrowserHistory } from "history";
+import ReactGA from "react-ga";
 import Home from "./pages/Home";
-
 import "./App.css";
+import { useEffect } from "react";
+
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_NO);
+const browserHistory = createBrowserHistory();
+browserHistory.listen((location, action) => {
+  ReactGA.pageview(location.pathname + location.search);
+});
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <Router>
       <div>
